462b34b332ac1b86e4287a81e8263adb6a92e443
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2023-05-23 12:47:20 -0400

    added bitmap library

--- demo/helloworld.cpp
+++ demo/helloworld.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 
 
+
+void PrintArray(int** arr, int w, int h)
+{
+    for (int i = 0; i < h; i += 1)
+    {
+        for (int j = 0; j < w; j += 1)
+        {
+            std::cout << arr[i][j];
+        }
+        std::cout << "\n";
+    }
+}
+
+void ModifyArray(int** arr, int w, int h)
+{
+    //int* test = (*(arr + ((h / 2) - 1)) + ((w / 2) - 1));
+    int** test = (arr + ((h / 2) - 1));
+    (*(test - 1))[w/2 - 2] = 2;
+    test[0][w/2] = 1;
+    PrintArray(arr, w, h);
+}
+
 int main(int argc, const char* argv[]) {
     std::cout << "Hello, World!\n";
+
+    const int w = 10;
+    const int h = 10;
+    int** arr;
+
+    arr = new int* [h];
+    for (int i = 0; i < h; i += 1)
+    {
+        arr[i] = new int[w];
+        for (int j = 0; j < w; j += 1)
+        {
+            arr[i][j] = 0;
+        }
+    }
+
+    ModifyArray(arr, w, h);
+
     return 0;
 }
---
+++ src/Environment.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Environment
+{
+private:
+	int TotalPixelAmount; //total num of pixels needed to draw the image 
+};
