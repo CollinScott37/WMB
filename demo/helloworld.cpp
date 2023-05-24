94a9d2b8f82059b5fe7068fb7e57ee4e01a02f08
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2023-05-24 14:37:40 -0400

    cleaned helloworld

--- demo/helloworld.cpp
+++ demo/helloworld.cpp
@@ -27,21 +27,6 @@ void ModifyArray(int** arr, int w, int h)
 
 int main(int argc, const char* argv[]) {
     std::cout << "Hello, World!\n";
-
-    const int w = 10;
-    const int h = 10;
-    int** arr;
-
-    arr = new int* [h];
-    for (int i = 0; i < h; i += 1)
-    {
-        arr[i] = new int[w];
-        for (int j = 0; j < w; j += 1)
-        {
-            arr[i][j] = 0;
-        }
-    }
-
     //ModifyArray(arr, w, h);
 
     Engine e;
