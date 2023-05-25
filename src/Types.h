205d477c3976e22b0e79e13970ae4a5b8bf71676
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2023-05-24 21:34:55 -0400

    Added a Location class

--- src/Types.h
+++ src/Types.h
@@ -9,6 +9,18 @@ namespace WMB
 {
 	typedef std::string string;
 	typedef std::vector <std::vector<Pixel>> Bmp;
+	
+	const Pixel BLACK = Pixel(0, 0, 0);
+
+	class Location
+	{
+	public:
+		int i;
+		int j;
+		Location() { i = -1; j = -1; }
+		Location(int li, int lj) { i = li; j = lj; }
+		
+	};
 
 	class Engine;
 	class Environment;
