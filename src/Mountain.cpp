5489d1906b13557683f5e781497b7ffa99f48a3f
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2023-05-24 21:35:49 -0400

    Mountain is a now able to draw moutains

--- src/Mountain.cpp
+++ src/Mountain.cpp
@@ -4,6 +4,7 @@ using namespace WMB;
 std::ostream& operator<<(std::ostream& out, Pixel const& data)
 {
 	return out << "(" << data.red << ", " << data.green << ", " << data.blue << ")";
+}
 
 std::ostream& operator<<(std::ostream& out, Location const& data)
 {
@@ -25,53 +26,150 @@ void Mountain::StartUp()
 
 
 
-void Mountain::Draw(Bmp& userBmp, Bmp& canvas)
+void Mountain::Draw(Bmp& userBmp, Bmp& canvas, int width, int height)
 {
 	std::vector<Pixel> uc;
 	
 	std::cout << "(Mountain) Started Drawing\n";
 
-	//std::cout << "" << userBmp[0][0];
-	for (int i = 0; i < userBmp.size(); i += 1)
+	//Loop throught the userBmp
+	for (int i = 0; i < height; i += 1)
 	{
-		for (int j = 0; j < userBmp[0].size(); j += 1)
+		for (int j = 0; j < width; j += 1)
 		{
-			//std::cout << "" << i << ", " << j << "\n";
 			
-			if (uc.size() == 0)
+			//tracks locations used for validation
+			std::vector<Location> locations;
+			bool isValid = ValidSpot(userBmp, canvas, i, j, locations);
+			if (isValid)
 			{
-				std::cout << "0: " << userBmp[i][j] << "\n";
-				uc.push_back(userBmp[i][j]);
-			}
-			else
-			{
-				bool found = false;
-				for (int h = 0; h < uc.size(); h += 1)
-				{
-					if (uc[h] == userBmp[i][j])
-					{
-						found = true;
-						break;
-					}
-				}
-				if (!found)
-				{
-					std::cout << "1: " << userBmp[i][j] << "\n";
-					uc.push_back(userBmp[i][j]);
-				}
+				//make sure no other Mountain use these pixels
+				MarkUsed(userBmp, locations);
+				
+				//draw the mountain
+				DrawImage(canvas, Location(i, j));
+				
+				//canvas[i][j] = Pixel(255, 0, 0); //Debug
 			}
 
+		}
+		
+	}
+	std::cout << "(Mountain) Done Drawing\n";
+}
 
+
+
+/**
+* Determines if this is a valid spot to put the Environment bmp on the canvas
+* if true, returns the locations of point on userBmp
+* if false, nothing (ish)
+*/
+bool Mountain::ValidSpot(Bmp& userBmp, Bmp canvas, int ci, int cj, std::vector<Location>& locations)
+{
+
+	int count = 0; //track number of matching colors
+	int s = size / 2;
+		
+	//start at the top row of the filter
+	for (int i = ci - s; i < ci + s; i += 1)
+	{
+		if (i < 0 || i >= userBmp.size()) { continue; } //invalid index
+		
+		//start at the left most column of the filter 
+		for (int j = cj - s; j < cj + s; j += 1)
+		{
+			if (j < 0 || j >= userBmp[0].size()) { continue; } //invalid index
+
+			//matches the designated color
 			if (userBmp[i][j] == color)
 			{
-				std::cout << "MATCH\n";
-				userBmp[i][j] = Pixel(-1,-1,-1);
-				canvas[i][j].red = 0;
+				count += 1;//inc
+				locations.push_back(Location(i, j)); //save location
+			}
 
+			//we don't want to any more pixels beyound the size
+			if (count == size)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+/**
+* flips all colors the locations used for validation in userBmp
+*/
+void Mountain::MarkUsed(Bmp& userBmp, std::vector<Location> locations)
+{
+	Location l;
+	for (int i = 0; i < locations.size(); i += 1)
+	{
+		l = locations[i];
+		userBmp[l.i][l.j] = ~userBmp[l.i][l.j];
+	}
+}
+
+/**
+* Draw the Mountain at the location on the canvas
+* location should be the center of the mountain bmp
+*/
+void Mountain::DrawImage(Bmp& canvas, Location location)
+{
+	//Get the bmp to draw
+	int index = rand() % bmps.size();
+
+	Bmp bmp = bmps[index];
+
+	//Get The Bounds
+	int height = bmp.size();
+	int width = bmp[0].size();
+	
+	//Convert from bmp space to canvas space
+	auto BmpToCanvas = [](int i, int j, int height, int width, Location location)
+	{
+		int x = location.i + (i - height / 2);
+		int y = location.j + (j - width / 2);
+
+		return Location(x, y);
+	};
+
+	//Check if valid canvas space pos
+	auto IsValidPos = [](Location location, Bmp canvas)
+	{
+		if (location.i < 0 || location.i >= canvas.size()) { return false; }
+		if (location.j < 0 || location.j >= canvas[0].size()) { return false; }
+		return true;
+	};
+
+
+	//std::cout << "MADE IT HERE\n";
+	//std::cout << "Center: " << location << "\n";
+	
+	//loop through the mountain bmp
+	for (int i = 0; i < height; i += 1)
+	{
+		for (int j = 0; j < width; j += 1)
+		{
+			
+			//Draw only the valid color 
+			if (bmps[index][i][j] == BLACK)
+			{
+				
+				//Location in the canvas
+				Location canvasPos = BmpToCanvas(i, j, height, width, location);
+				
+				//std::cout << "canvas Location: " << canvasPos;
+				
+				bool isValid = IsValidPos(canvasPos, canvas);
+				if (isValid)
+				{
+					canvas[canvasPos.i][canvasPos.j] = BLACK;
+				}
 			}
 		}
 	}
 
-	std::cout << "(Mountain) Done Drawing\n";
 }
 
--- src/Mountain.h
+++ src/Mountain.h
@@ -9,11 +9,15 @@ namespace WMB
 	{
 	public:
 		void StartUp();
-		void Draw(Bmp& userBmp, Bmp& canvas);
+		void Draw(Bmp& userBmp, Bmp& canvas, int width, int height);
+		bool ValidSpot(Bmp& userBmp, Bmp canvas, int ci, int cj, std::vector<Location>& locations);
+		void MarkUsed(Bmp& userBmp, std::vector<Location> locations);
+		void DrawImage(Bmp& canvas, Location location);
 		Mountain()
 		{
 			path = ppath + "Mountains/";
 			color = Pixel(194, 33, 229);
+			size = 5;
 		}
 	};
 }
