889af748686831099327435de9c5af2d29bcb89b
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2023-06-21 17:26:05 -0400

    Added DeepWater Class

--- src/Water.cpp
+++ src/Water.cpp
@@ -185,3 +185,309 @@ void Water::DrawWaterBorder(Bmp& userBmp, Bmp& canvas, int height, int width)
 		}
 	}
 }
+
+
+
+void DeepWater::StartUp()
+{
+	std::cout << "(DeepWater) Starting Up\n";
+	Bitmap image;
+	Bitmap b;
+
+	LoadTiles(path, image, tiles);
+	//std::cout << "color: " << color << "\n";
+
+
+	std::cout << "(DeepWater) Start Complete\n";
+}
+
+
+void DeepWater::Draw(Bmp& userBmp, Bmp& canvas, int width, int height)
+{
+	std::cout << "(DeepWater) Started Drawing\n";
+	std::cout << "# of borders " << borders.size() << "\n";
+	std::vector<Location> locations = FindAllDeepWater(userBmp, width, height);
+	std::cout << "Found " << locations.size() << " Deepwaters\n";
+	std::vector<std::vector<Location>> clusters = ClusterDeepWater(locations);
+	std::cout << "Formed " << clusters.size() << " clusters\n";
+
+
+	DrawCluster(clusters, canvas);
+
+	std::vector<Location> centers = GetClusterCenter(clusters);
+
+	std::vector< std::pair<Location, std::pair<int, int>>> cluster_width_height = CalcClusterSize(userBmp, width, height, clusters, centers);
+	
+	std::cout << "(DeepWater) Done Drawing\n";
+
+}
+
+//Calculate the Size for a tile to fit into for each cluster
+std::vector < std::pair<Location, std::pair<int, int>>> DeepWater::CalcClusterSize(Bmp& userBmp, int width, int height, std::vector<std::vector<Location>> clusters, std::vector<Location> centers)
+{
+	std::vector < std::pair<Location, std::pair<int, int>>> cluster_width_height;
+
+	//find the width and height of each cluster starting from the centers
+	for (int u = 0; u < clusters.size(); u += 1)
+	{
+		const auto& center = centers[u];
+		int width_count = 0;
+		int height_count = 0;
+
+		//going left
+		for (int j = center.j; j > 0; j -= 1)
+		{
+			Location l = Location(center.i, j);
+			for (const auto& loc : clusters[u])
+			{
+				if (l == loc)
+				{
+					width_count += 1;
+				}
+			}
+		}
+
+		//going right
+		for (int j = center.j; j < width; j += 1)
+		{
+			Location l = Location(center.i, j);
+			for (const auto& loc : clusters[u])
+			{
+				if (l == loc)
+				{
+					width_count += 1;
+				}
+			}
+		}
+
+		//going up
+		for (int i = center.i; i > 0; i -= 1)
+		{
+			Location l = Location(i, center.j);
+			for (const auto& loc : clusters[u])
+			{
+				if (l == loc)
+				{
+					height_count += 1;
+				}
+			}
+		}
+
+		//going down
+		for (int i = center.i; i < height; i += 1)
+		{
+			Location l = Location(i, center.j);
+			for (const auto& loc : clusters[u])
+			{
+				if (l == loc)
+				{
+					height_count += 1;
+				}
+			}
+		}
+
+  std::pair<int,int> w_h(width_count, height_count);
+  std::pair<Location, std::pair<int,int>> c_w_h(center, w_h);
+
+		cluster_width_height.push_back(c_w_h);
+	}
+
+	for (const auto& c_w_h : cluster_width_height)
+	{
+		std::cout << "Center: " << c_w_h.first << " width: " << c_w_h.second.first << " height: " << c_w_h.second.second << "\n";
+	}
+
+	return cluster_width_height;
+}
+
+void DeepWater::DrawCluster(std::vector<std::vector<Location>> clusters, Bmp& canvas)
+{
+	int i = 0;
+	for (const auto& cluster : clusters)
+	{
+		std::cout << "i: " << i << "\n";
+		int amount = ((255 / clusters.size()) * i);
+		std::cout << "amount: " << amount << "\n";
+		for (const auto& loc : cluster)
+		{
+			canvas[loc.i][loc.j] = Pixel(255 - amount, 0, 0);
+		}
+		i += 1;
+	}
+
+	//TODO: REMOVE
+	std::vector<Location> centers = GetClusterCenter(clusters);
+
+	//Draw Centers of each clusters
+	for (const auto& loc : centers)
+	{
+		std::cout << "loc: " << loc << "\n";
+		canvas[loc.i][loc.j] = Pixel(0, 0, 0);
+	}
+
+}
+
+//Finds the center of each cluster
+std::vector<Location> DeepWater::GetClusterCenter(std::vector<std::vector<Location>> clusters)
+{
+	std::vector<Location> centers;
+
+	//loop through each cluster
+	for (const auto& cluster : clusters)
+	{
+		Location center = Location(0, 0);
+		
+		//Loop through all locations in the cluster
+		for (const auto& loc : cluster)
+		{
+			center += loc;
+			
+		}
+		center /= (int)cluster.size();
+		
+		centers.push_back(center);
+	}
+
+	return centers;
+}
+
+
+
+/*
+* Returns a list of Water tiles that are Deep
+* 
+* Criteria to be DeepWater:
+* Must be X pixels away from any non water blocks
+*/
+
+std::vector<Location> DeepWater::FindAllDeepWater(Bmp userBmp, int width, int height)
+{
+	std::vector<Location> locations;
+
+	//loop through the userBmp
+	for (int i = 0; i < height; i += 1)
+	{
+		
+    for (int j = 0; j < width; j += 1)
+		{
+		
+      //must be water block
+			if (userBmp[i][j] == color || userBmp[i][j] == ~color)
+			{
+				Location loc = Location(i, j);
+				 
+				float dist = 100;
+				
+        //find distance to land from curr position
+				for (const auto& border : borders)
+				{
+					float newDist = abs(Dista(loc, border));
+					if (newDist < dist) { dist = newDist; }
+				}
+
+        //if the smallest distance from land is farther than min dist
+				if (dist >= distance)
+				{
+					locations.push_back(loc);
+				}
+			}
+		}
+	}
+
+	return locations;
+}
+
+
+
+
+//Return Clusters of DeepWaters
+std::vector<std::vector<Location>> DeepWater::ClusterDeepWater(std::vector<Location> deepWaters)
+{
+	//clusters to return
+	std::vector<std::vector<Location>> clusters;
+	
+	//make each element in the things a cluster
+	for (int i = 0; i < deepWaters.size(); i += 1)
+	{
+    //make empty vector
+		std::vector<Location> loc;
+		
+    //store the curr location to it
+    loc.push_back(deepWaters[i]);
+	  
+    //add to clusters
+    clusters.push_back(loc);
+	}
+
+
+	//Determine if any two points in the clusters are close
+	auto ClusterClose = [&](std::vector<Location> c1, std::vector<Location> c2)
+	{
+		//if either clusters are empty
+		if (c1.size() == 0 || c2.size() == 0) { return false; }
+		for (int i = 0; i < c1.size(); i += 1)
+		{
+			for (int j = 0; j < c2.size(); j += 1)
+			{
+				if (abs(Dista(c1[i], c2[j])) <= 1.0f)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	};
+
+
+	bool changed = false;
+	
+	//loop until no new clusters are made
+	while (true)
+	{
+    
+    //track if a cluster was formed
+    changed = false;
+		
+    //form clusters
+		for (int i = 0; i < clusters.size(); i += 1)
+		{
+	
+      for (int j = i+1; j < clusters.size(); j += 1)
+			{
+				
+        //see if either of the clusters are close to eachother
+        if (ClusterClose(clusters[i], clusters[j]))
+				{
+					
+          //add points from one cluster to another
+					clusters[i].insert(clusters[i].end(), clusters[j].begin(), clusters[j].end());
+					clusters[j].clear();
+					changed = true;
+				
+        }
+			}
+		}
+    
+    //if true - no clusters were formed
+    if (!changed)
+		{
+			break;
+		}
+
+  }
+
+  //remove all clusters that have nothing in them	
+  std::vector<std::vector<Location>> clusters_slimed;
+	for (const auto& c : clusters)
+	{
+		if (c.size() > 0)
+		{
+			clusters_slimed.push_back(c);
+		}
+	}
+
+	return clusters_slimed;
+
+}
+
+
--- src/Water.h
+++ src/Water.h
@@ -3,6 +3,49 @@
 
 namespace WMB
 {
+	
+	class DeepWater : Environment
+	{
+	public:
+		void StartUp();
+		void Draw(Bmp& userbmp, Bmp& canvas, int width, int height);
+		
+		//a list of locations that mark the border between water and land
+		std::vector<Location> borders;
+
+		DeepWater()
+		{
+			color = Pixel(0, 161, 255);
+			distance = 10.0f;
+		}
+
+		DeepWater(float dist)
+		{
+			distance = dist;
+		}
+
+	private:
+		string path = "Water/Creatures";
+		float distance; //distance from the land
+			
+		//Returns a list of Water tiles that are Deep
+    //Location of DeepWater, distnce from 
+		std::vector<Location> FindAllDeepWater(Bmp userBmp, int width, int height);
+		
+		//Return Clusters of DeepWaters
+		std::vector<std::vector<Location>> ClusterDeepWater(std::vector<Location> deepWaters);
+	
+		//DEBUG: Draws the clusters onto the canvas
+		void DrawCluster(std::vector<std::vector<Location>> clusters, Bmp& canvas);
+
+		//Finds the center of each cluster
+		std::vector<Location> GetClusterCenter(std::vector<std::vector<Location>> clusters);
+
+		//Calculate the Size for a tile to fit into for each cluster
+		std::vector < std::pair<Location, std::pair<int, int>>> CalcClusterSize(Bmp& userBmp, int width, int height, std::vector<std::vector<Location>> clusters, std::vector<Location> centers);
+
+	};
+
 	class Water : public Environment
 	{
 
@@ -18,4 +61,5 @@ namespace WMB
 			size = 8;
 		}
 	};
+
 }
